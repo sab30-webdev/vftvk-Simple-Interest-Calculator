a621e2e6222dca8c373117a6e74ae914140b0368
sab30-webdev <sabarinath3072001@gmail.com>
2021-03-31 18:12:02 +0530

    v1

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+let p = document.getElementById("principal").value;
+let r = document.getElementById("rate").value;
+let y = document.getElementById("years").value;
+
+function updateVal(val) {
+  p = parseInt(val);
 }
-        
+
+function displaySlider(val) {
+  document.getElementById("slidervalue").innerHTML = val + " %";
+  r = parseFloat(val);
+}
+
+function updateDrop(val) {
+  y = parseInt(val);
+}
+
+function compute() {
+  if (p <= 0) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+  } else {
+    let interest = (p * y * r) / 100;
+    var d = new Date();
+    var n = d.getFullYear();
+    let result = `If you deposit ${p},
+      at an interest rate of ${r}%.
+      You will receive an amount of ${interest},
+      in the year ${n + y}`;
+    document.getElementById("result").innerHTML = result;
+  }
+}
